0f52c492eb0ce117682b755124da10e31eb84d8f
Assisneto Damasceno <netodamasceno12@gmail.com>
2025-10-02 00:02:45 -0300

    m5

---
+++ apps/expo/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from 'expo-router'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#007AFF',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: 'TMS - Todos' }} />
+      </Stack>
+    </GestureHandlerRootView>
+  )
+}
