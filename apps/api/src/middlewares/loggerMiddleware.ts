ee9acd3283e6a74855971a6126e5bf11183dbbd4
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2025-10-13 11:01:15 -0300

    feat(logger): add logger for api and db

---
+++ apps/api/src/middlewares/loggerMiddleware.ts
@@ -0,0 +1,28 @@
+import Elysia from 'elysia'
+import { serializeError } from 'serialize-error'
+import { logger } from '../logger'
+
+export const loggerMiddleware = new Elysia({ name: 'logger' })
+  .derive({ as: 'global' }, () => ({ startTime: performance.now() }))
+  .onAfterResponse(
+    { as: 'global' },
+    ({ request, body, path, set, startTime, headers, response, error }) => {
+      const responseTime = performance.now() - startTime
+
+      const logData = {
+        req: {
+          headers: headers,
+          body: body,
+        },
+        res: {
+          headers: set.headers,
+          body: response,
+          statusCode: set.status,
+        },
+        error: serializeError(error, { maxDepth: 1 }),
+        responseTime,
+      }
+
+      logger.info(logData, `${request.method} ${path} - ${set.status}`)
+    }
+  )
